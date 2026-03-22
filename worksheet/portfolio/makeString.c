d33aebc6ac119587fcaa9ab2e15f8403076a3289
Splixson72 <145981456+Splixson72@users.noreply.github.com>
2026-03-22 13:42:54 +0000

    worksheet

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,6 +1,6 @@
 
 /*
-Name:
+Name:Zaryaab Ullah
 Student ID:
 */
 
@@ -8,12 +8,15 @@ Student ID:
 #include <string.h>
 
 int main( int argc, char **argv ) {
-
     // define appropriate data to hold your answer
-
+    char input[100] = "";
     // process the command-line data using appropriate string functions
-
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
-
-    return 0;
+    for (int i = 1; i < argc; i++) { //goes though number of arguments
+        strcat(input, argv[i]); //concatenation
+        if (i < argc - 1) {
+            strcat(input, "-"); //adds hyphens in between
+        }
+    }
+    // use only this single print statement in your submitted code
+    printf("%s\n", input);
 }
